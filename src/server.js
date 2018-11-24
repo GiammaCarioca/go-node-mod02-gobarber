654610977851238791d0352dcaf85c24521619db
Thiago Giammattey <giammattey@gmail.com>
2018-11-24 19:03:58 -0200

    sessão

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
 const nunjucks = require('nunjucks')
 const path = require('path')
 
@@ -13,8 +15,18 @@ class App {
   }
 
   middlewares () {
-    this.express.use(express.static(path.resolve(__dirname, 'public')))
     this.express.use(express.urlencoded({ extended: false }))
+    this.express.use(
+      session({
+        name: 'root',
+        secret: 'MyAppSecret',
+        resave: true,
+        store: new FileStore({
+          path: path.resolve(__dirname, '..', 'tmp', 'sessions')
+        }),
+        saveUninitialized: true
+      })
+    )
   }
 
   views () {
@@ -24,6 +36,7 @@ class App {
       autoescape: true
     })
 
+    this.express.use(express.static(path.resolve(__dirname, 'public')))
     this.express.set('view engine', 'njk')
   }
 
